a4084b48fdef54dcf34154cd55280249b9d35f57
paisen4312 <syvlebgth@gmail.com>
2020-11-16 21:58:12 +0900

    ルーティング４

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/top' => "home#top"
-  get 'https://paisen4312.github.io/satsukichi/' => "home#top"
+  get 'https://paisen4312.github.io/satsukichi' => "home#top"
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
